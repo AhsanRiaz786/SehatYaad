182c8e784d4b53f80a5a817dcc865deda46723c2
Ahsan Riaz <ahsanriaz8000@gmail.com>
2025-11-30 00:11:52 +0500

    Add index file to export screen components
    - Created an index.ts file to streamline the export of various screen components including AddMedicationScreen, HomeScreen, InsightsScreen, MedicationDetailScreen, OnboardingScreen, SettingsScreen, and SplashScreen.
    - This change enhances the organization and accessibility of screen components within the application.

---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+export { default as AddMedicationScreen } from './AddMedicationScreen';
+export { default as HomeScreen } from './HomeScreen';
+export { default as InsightsScreen } from './InsightsScreen';
+export { default as MedicationDetailScreen } from './MedicationDetailScreen';
+export { default as OnboardingScreen } from './OnboardingScreen';
+export { default as SettingsScreen } from './SettingsScreen';
+export { default as SplashScreen } from './SplashScreen';
